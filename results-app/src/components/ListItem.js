3a0622a3872a984bdaf336c555441c3bdc69feb1
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-02 14:31:26 -0800

    added jsx class names

--- results-app/src/components/ListItem.js
+++ results-app/src/components/ListItem.js
@@ -4,9 +4,9 @@ export default function ListItem(props){
     // returns all jsx content for one list item
     return (
         <div className ="list-item">
-        <p>{props.title}</p> 
-        <p>{props.summary}</p>
-        <p>{props.difficulty}</p>
+        <p className ='result-title'>{props.title}</p> 
+        <p className = 'result-summary'>{props.summary}</p>
+        <p className = 'result-difficulty'>{props.difficulty}</p>
         </div>
     )
 }
