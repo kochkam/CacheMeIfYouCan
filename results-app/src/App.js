880d9ae64be7aa86fadf37017f2efd23d49090e3
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-02 14:20:19 -0800

    added comments for functions

--- results-app/src/App.js
+++ results-app/src/App.js
@@ -9,20 +9,26 @@ var currentSearch = createResults();
 
 function App() {
   return (
+
+    // Build results list    
     // for each hike in currentSearch.results render ListItem
-    currentSearch.results.map(
+    <div className = 'results-list'>
+    {currentSearch.results.map(
       hike => (
         <ListItem key = {hike.id} title = {hike.title} summary = {hike.summary} difficulty= {hike.difficulty}/>
       )
-    )
+    )}
+    </div>
+
   )
 }
 
 // Function fills a SearchResults object's results list with 10 hike objects and returns SearchResults object
+// THIS FUNCTION TO BE REPLACED WITH FUNCTION THAT PERFORMSACTUAL SEARCH REQUEST FROM BACKEND
 function createResults(){
   var searchResults = new SearchResults();
     for (var i = 1; i <= 10; i++){
-        searchResults.results.push(new Hike(i, "Hike: "+ i, "This is the summary for Hike: " + i, "difficulty: " + i));
+        searchResults.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike: " + i, "difficulty: " + i));
     }
   return searchResults
 }
--- results-app/src/components/ListItem.js
+++ results-app/src/components/ListItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function ListItem(props){
-    // returns all content for one list item
-    return <div className ="List-item">
+    // returns all jsx content for one list item
+    return <div className ="list-item">
         <p>{props.title}</p> 
         <p>{props.summary}</p>
         <p>{props.difficulty}</p>
