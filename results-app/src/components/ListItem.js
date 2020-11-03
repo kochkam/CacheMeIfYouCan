40a9e115eff7070e3478c0dec2fada70f1175774
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-02 16:01:21 -0800

    list item styles

--- results-app/src/components/ListItem.js
+++ results-app/src/components/ListItem.js
@@ -4,11 +4,17 @@ export default function ListItem(props){
     // returns all jsx content for one list item
     return (
         <div className ="list-item">
-        <img className = 'result-img' src = {props.img}/>
-        <p className ='result-title'>{props.title}</p> 
+            <img className = 'result-img' src = {props.img}/>
+            <span className ='result-title'>
+                <h3 className = 'result-title-text'>{props.title}</h3> 
+            </span>
+        
+        <span className = 'result-act-distance'>
+            <p className = 'result-activity'>Activity Level: {props.activityLevel}</p>
+            <p className = 'result-distance'>Distance: {props.distance}</p>
+        </span>
         <p className = 'result-summary'>Summary: {props.summary}</p>
-        <p className = 'result-activity'>Activity Level: {props.activityLevel}</p>
-        <p className = 'result-distance'>Distance: {props.distance}</p>
+
         <p className = 'result-temp'>Current Temp: {props.temp}</p>
         
         </div>
