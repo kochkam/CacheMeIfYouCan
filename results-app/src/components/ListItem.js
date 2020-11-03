57da8a94a828a125191873b70aff89b86fecff19
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-02 17:05:10 -0800

    formatting changes to ListItem

--- results-app/src/components/ListItem.js
+++ results-app/src/components/ListItem.js
@@ -9,11 +9,11 @@ export default function ListItem(props){
             <div className="result-text">
                 <h3 className = 'result-title-text'>{props.title}</h3>
                 <div className= "result-detail-text">
-                    <div className= "result-act-dist">
+                    <span className= "result-act-dist">
                     <p className = 'result-activity'>Activity Level: {props.activityLevel}</p>
                     <p className = 'result-distance'>Distance: {props.distance}</p>
-                    </div>
-                    <p className = 'result-summary'>Summary: {props.summary}</p>  
+                    </span>
+                    <span className = 'result-summary'>Summary: {props.summary}</span>  
                 </div>
             </div>
             <p className = 'result-temp'>Current Temp: {props.temp}</p>
