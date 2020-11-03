ac5d12c9ecb6d556095332416c49464dc0714ec9
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-02 16:56:53 -0800

    formatting changes to ListItem

--- results-app/src/components/ListItem.js
+++ results-app/src/components/ListItem.js
@@ -5,18 +5,18 @@ export default function ListItem(props){
     return (
         <div className ="list-item">
             <img className = 'result-img' src = {props.img}/>
-            <span className ='result-title'>
-                <h3 className = 'result-title-text'>{props.title}</h3> 
-            </span>
-        
-        <span className = 'result-act-distance'>
-            <p className = 'result-activity'>Activity Level: {props.activityLevel}</p>
-            <p className = 'result-distance'>Distance: {props.distance}</p>
-        </span>
-        <p className = 'result-summary'>Summary: {props.summary}</p>
-
-        <p className = 'result-temp'>Current Temp: {props.temp}</p>
-        
+                 
+            <div className="result-text">
+                <h3 className = 'result-title-text'>{props.title}</h3>
+                <div className= "result-detail-text">
+                    <div className= "result-act-dist">
+                    <p className = 'result-activity'>Activity Level: {props.activityLevel}</p>
+                    <p className = 'result-distance'>Distance: {props.distance}</p>
+                    </div>
+                    <p className = 'result-summary'>Summary: {props.summary}</p>  
+                </div>
+            </div>
+            <p className = 'result-temp'>Current Temp: {props.temp}</p>
         </div>
     )
 }
