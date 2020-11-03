3f02e0d06d36c640e3e25a816669ddd34ec9c559
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-03 15:45:28 -0800

    initial commit

---
+++ adv-search-app/src/components/DetailView/DetailView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DetailView.css';
+
+export default function DetailView() {
+    return(
+        <div className="detail-view">
+            <h1>Detail View</h1>
+        </div>
+    )
+}
