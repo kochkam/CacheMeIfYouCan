41252c3c430da8340b7692d605274d1437bf7019
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-04 08:40:04 -0800

    implemented results list

--- trails-app/src/App.js
+++ trails-app/src/App.js
@@ -3,21 +3,14 @@ import Navbar from './components/Navbar/Navbar';
 import AdvSearch from './components/AdvSearch/AdvSearch'
 import DetailView from './components/DetailView/DetailView'
 import {Route, BrowserRouter} from "react-router-dom";
-import LinkButton from "./components/LinkButton";
 import Home from "./views/Home";
 import ResultsList from "./views/ResultsList";
 import UserProfile from "./views/UserProfile";
-import SearchForm from './components/SearchForm';
-import homeicon from './images/home.png';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className="search-container">
-        <h1>Find your next trail....</h1>
-        <SearchForm/>
-      </div>
       <div className='base-container'>
         <div className="content">
           <Route exact path="/" component={Home}/>
--- trails-app/src/views/ResultsList.js
+++ trails-app/src/views/ResultsList.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
- 
+import SearchResults from '../classes/SearchResults'
+import ListItem from '../components/ListItem'
+import Hike from '../classes/Hike';
+
+// currentSearch object which contains assembled list of hikes
+var currentSearch = createResults();
+
+// Function fills a SearchResults object's results list with 10 hike objects and returns SearchResults object
+// THIS FUNCTION TO BE REPLACED WITH FUNCTION THAT PERFORMS ACTUAL SEARCH REQUEST FROM BACKEND
+function createResults(){
+  var searchResults = new SearchResults();
+    for (var i = 1; i <= 10; i++){
+        searchResults.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike " + i, i, "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1078&q=80", i, i));
+    }
+  return searchResults
+}
+
+// Function is triggered when hike is clicked and id of hike is passed in
+// TRIGGER RENDERING DETAIL VIEW HERE AND HIDE RESULTS LIST
+function hikeClick(hikeID){
+  console.log("clicked Hike: " + hikeID);
+}
+
 class ResultsList extends Component {
   render() {
     return (
-      <div className='results'>
-          <h2>Results List Placeholder</h2>
-          <ul>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-            <li>placeholder</li>
-          </ul>
 
+      // Build results list    
+      // for each hike in currentSearch.results render ListItem
+      <div className = 'results-list'>
+      {currentSearch.results.map(
+        hike => (
+          <ListItem id = {hike.id} title = {hike.title} summary = {hike.summary} activityLevel = {hike.activityLevel} img = {hike.imgURL} distance = {hike.distance} temp = {hike.temp} clickFunction = {hikeClick}/>
+        )
+      )}
       </div>
+  
     );
   }
 }
