9252f041acc9fb0eae1646ec5e058dcb87c2917f
drizzit <mjkochka@gmail.com>
2020-11-04 14:05:26 -0800

    made minor edits since pages load locally but not on the heroku server

--- trails-app/server/server.js
+++ trails-app/server/server.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath))
 
-app.get('/*', function (req,res) {
+app.get('*', function (req,res) {
     res.sendFile(path.join(publicPath, index.html));
 });
 
