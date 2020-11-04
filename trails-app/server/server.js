d06edde1ee3e5896c14853859ea7e707110edae3
drizzit <mjkochka@gmail.com>
2020-11-04 13:37:16 -0800

    made minor edits since pages load locally but not on the heroku server

--- trails-app/server/server.js
+++ trails-app/server/server.js
@@ -2,12 +2,12 @@ var path = require("path");
 var express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'build')
+const publicPath = path.join(__dirname, 'build')
 const port = process.env.Port || 3001
 
 app.use(express.static(publicPath));
 
-app.get('/', function (req,res) {
+app.get('/*', function (req,res) {
     res.sendFile(path.join(publicPath,'index.html'))
 });
 
