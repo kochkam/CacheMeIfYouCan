51d88de3e2e1f858dcd18f7f18c1be7e4f13204a
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-05 11:48:34 -0800

    server port fix

--- trails-app/server/server.js
+++ trails-app/server/server.js
@@ -14,7 +14,7 @@ app.get('/*', function (req,res) {
 });
 
 
-const port = process.env.PATH || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,() => {
     console.log('Server Up!');
