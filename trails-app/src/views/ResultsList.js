2405988a94ac6d5755e896716a23bcbeef08ea4c
drizzit <mjkochka@gmail.com>
2020-11-04 19:23:29 -0800

    Added functions translateZip and getHikeData to make API calls that return JSON objs. Comments contain API links for documentation info.

--- trails-app/src/views/ResultsList.js
+++ trails-app/src/views/ResultsList.js
@@ -5,9 +5,64 @@ import Hike from '../classes/Hike';
 import LinkButton from "../components/LinkButton";
 import { useParams } from 'react-router-dom';
 
+
+function translateZip(zip) { // Zip to location information api info can be found here: zipcodeapi.com/API#zipToLoc
+
+  var apiURL = "http://www.zipcodeapi.com/rest/gPpQfeppCMSXxaOPsb6MgxfBYNoxj6hb9bVFua9rKbqMRCnFE89VMZyWexRmKoRw/info.json/" + zip + "/degrees";
+
+  let req = new XMLHttpRequest();
+
+  req.open('GET', apiURL, true);
+
+  req.addEventListener('load', function () {
+
+    if (req.status >= 200 && req.status < 400) {
+      var response = JSON.parse(req.responseText);
+      return response //return lat and long to caller
+
+    }
+    else {
+      console.log("error in network request: " + req.statusText);
+    }
+  });
+
+  req.send(null)
+
+}
+
+function getHikeData(zip){
+
+  obj = translateZip(zip); //translate zip and return JSON obj
+  apiKey = "200964805-fbbd50c01b329d117306d1834dfd6a2d";
+  maxDistance = "&maxDistance=20"
+  lat = obj.lat //get lat and long
+  lon = obj.lng
+
+  let req = new XMLHttpRequest();
+  url = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon" + maxDistance + apiKey  // api info can be found here: https://www.hikingproject.com/data#_=_
+
+  req.open('GET', url, true)
+
+  req.addEventListener('load', function () {
+
+    if (req.status >= 200 && req.status < 400) {
+      var HikeData = JSON.parse(req.responseText);
+      return HikeData // use HikeData.trails[i].<attr name> to access the attribute you need. Results returned will be 10. 
+
+    }
+    else {
+      console.log("error in network request: " + req.statusText);
+    }
+  });
+
+  req.send(null)
+
+}
+
 // Function fills a SearchResults object's results list with 10 hike objects and returns SearchResults object
 // THIS FUNCTION TO BE REPLACED WITH FUNCTION THAT PERFORMS ACTUAL SEARCH REQUEST FROM BACKEND
 function createResults(){
+  hikeDataArray = getHikeData(zip)
   var searchResults = new SearchResults();
     for (var i = 1; i <= 10; i++){
         searchResults.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike " + i, i, "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1078&q=80", i, i));
@@ -23,9 +78,9 @@ function hikeClick(hikeID){
 
 export default function ResultsList() {
   // currentSearch object which contains assembled list of hikes
-  var currentSearch = createResults();
-
   let {zip} = useParams();
+  var currentSearch = createResults(zip);
+
   return (
 
     // Build results list    
