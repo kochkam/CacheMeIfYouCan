57f092e4e86c39eaeea18fab5eb56070f0c7af19
drizzit <mjkochka@gmail.com>
2020-11-04 16:40:47 -0800

    made minor edits since pages load locally but not on the heroku server

--- trails-app/server/server.js
+++ trails-app/server/server.js
@@ -11,6 +11,9 @@ app.get('/*', function (req,res) {
 });
 
 
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000); 
+app.listen(port,() => {
+    console.log('Server Up!');
+});
 
