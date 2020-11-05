5435c37a8b0b63b4236770298528b20f8b10798a
drizzit <mjkochka@gmail.com>
2020-11-05 14:15:45 -0800

    making functions asynch

--- trails-app/src/classes/SearchResults.js
+++ trails-app/src/classes/SearchResults.js
@@ -20,7 +20,7 @@ class SearchResults{
         let req = new XMLHttpRequest();
         let url = "https://www.hikingproject.com/data/get-trails?lat=" + this.lat + "&lon" + this.long + maxDistance + apiKey;  // api info can be found here: https://www.hikingproject.com/data#_=_
 
-        req.open('GET', url, true);
+        req.open('GET', url, false);
 
         req.addEventListener('load', function () {
 
