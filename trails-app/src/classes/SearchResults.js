b521abb2e7a00a1881d98f7a956f2424162fae05
drizzit <mjkochka@gmail.com>
2020-11-05 21:33:41 -0800

    editing hikedata function to access correct data

--- trails-app/src/classes/SearchResults.js
+++ trails-app/src/classes/SearchResults.js
@@ -19,42 +19,32 @@ class SearchResults{
         let apiKey = "&key=200964805-fbbd50c01b329d117306d1834dfd6a2d";
         let maxDistance = "&maxDistance=20";
 
-        let req = new XMLHttpRequest();
         let url = "https://www.hikingproject.com/data/get-trails?lat=" + this.lat + "&lon=" + this.long + maxDistance + apiKey;  // api info can be found here: https://www.hikingproject.com/data#_=_
 
-        req.open('GET', url, false);
 
-        req.addEventListener('load', function () {
+        let obj;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => obj = data)
+            .then(() => obj)
+        // build hikes into results
+        for (var i = 0; i < response.length; i++) {
+            var hike = new Hike();
+            hike.id = response[i].id;
+            hike.index = i;
+            hike.title = response[i].name;
+            hike.summary = response[i].summary;
+            hike.activityLevel = response[i].difficulty;
+            hike.imgURL = response[i].imgSmall;
+            hike.largeimgURL = response[i].imgMedium;
+            // attribute is titled "length" from api for hike distance. javascript doesnt like this
+            hike.distance = response[i].length;
+            hike.long = response[i].longitude;
+            hike.lat = response[i].latitude;
+            // add hike object to results
+            this.results.push(hike);
+        }
 
-            if (req.status >= 200 && req.status < 400) {
-                // build hikes into results
-                var response = JSON.parse(req.responseText);
-                for (var i = 0; i < response.length; i++){
-                    var hike = new Hike();
-                    hike.id = response[i].id;
-                    hike.index = i;
-                    hike.title = response[i].name;
-                    hike.summary = response[i].summary;
-                    hike.activityLevel = response[i].difficulty;
-                    hike.imgURL = response[i].imgSmall;
-                    hike.largeimgURL = response[i].imgMedium;
-                    // attribute is titled "length" from api for hike distance. javascript doesnt like this
-                    hike.distance = response[i].length;
-                    hike.long = response[i].longitude;
-                    hike.lat = response[i].latitude;
-                    // add hike object to results
-                    this.results.push(hike);
-
-                }
-                // use HikeData.trails[i].<attr name> to access the attribute you need. Results returned will be 10. 
-            }
-            else {
-                console.log("error in network request: " + req.statusText);
-            }
-        });
-
-        req.send(null);
-        Event.preventDefault()
 
     }
 
