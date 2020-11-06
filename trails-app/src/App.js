11c3692417f1c0ed4ff63e276c7c87544c5caa11
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-06 14:06:56 -0800

    testing async for resultslist

--- trails-app/src/App.js
+++ trails-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {Route, BrowserRouter} from "react-router-dom";
 import Home from "./views/Home";
-import ResultsList from "./views/ResultsList";
+import TestList from "./views/ResultsList";
 import UserProfile from "./views/UserProfile";
 import DetailView from "./views/DetailView";
 import SearchResults from "./classes/SearchResults.js";
@@ -18,7 +18,7 @@ export default function App() {
         <div className="content">
           <Route exact path="/" children={<Home searchObj = {currentSearch} />}/>
           <Route path="/user-profile" component={UserProfile}/>
-          <Route path="/results-list/:zip" children={<ResultsList searchObj = {currentSearch} />}/>
+          <Route path="/results-list/:zip" component={<TestList searchObj = {currentSearch} />}/>
           <Route path="/detail-view/:hike_id" children={<DetailView searchResult = {currentSearch.results}/>}/>
         </div>
         
--- trails-app/src/views/ResultsList.js
+++ trails-app/src/views/ResultsList.js
@@ -13,41 +13,54 @@ function hikeClick(hikeID){
   console.log("clicked Hike: " + hikeID);
 }
 
-export default function ResultsList(props) {
-
-
-  // currentSearch object from props which contains assembled list of hikes
-  let {zip} = useParams();
-
-  // TEMPORARY BUILDING OF RESULTS - This will need to happen during the search
-
-  props.searchObj.translateZip(zip);
-
-  //for (var i = 1; i <= 10; i++){
-  //  props.searchObj.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike " + i, i, "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1078&q=80", i, i));
-  //}
-  if (props.searchObj.results.length == 0){
-    return (
-      <div className = 'results-list'>
-        <p>No Results Found</p>
-      </div>
-    );
+class TestList extends React.Component{
+  constructor(props){
+    super(props);
+    // currentSearch object from props which contains assembled list of hikes
+    this.zip = 95050;
+    this.state = {loaded: false};
+    console.log(props);
   }
-  else {
-    return (
 
-      // Build results list    
-      // for each hike in currentSearch.results render ListItem
-      <div className = 'results-list'>
-      {props.searchObj.results.map(
-        hike => (
-          <LinkButton exact to={"/detail-view/" + hike.id}>
-            <ListItem id = {hike.id} title = {hike.title} summary = {hike.summary} activityLevel = {hike.activityLevel} img = {hike.imgURL} distance = {hike.distance} temp = {hike.temp} clickFunction = {hikeClick}/>
-          </LinkButton>
-        )
-      )}
-      </div>
-    );
+  render() {
+    // TEMPORARY BUILDING OF RESULTS - This will need to happen during the search
+    this.props.searchObj.translateZip(this.zip).then(() => {
+      this.setState({loaded: true});
+    });
+    const loaded = this.state.loaded;
+    let toRender;
+    if(loaded){
+      //for (var i = 1; i <= 10; i++){
+      //  props.searchObj.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike " + i, i, "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1078&q=80", i, i));
+      //}
+      if (this.props.searchObj.results.length == 0){
+        toRender = (
+          <div className = 'results-list'>
+            <p>No Results Found</p>
+          </div>
+        );
+      }
+      else {
+        toRender = (
+
+          // Build results list    
+          // for each hike in currentSearch.results render ListItem
+          <div className = 'results-list'>
+          {this.props.searchObj.results.map(
+            hike => (
+              <LinkButton exact to={"/detail-view/" + hike.id}>
+                <ListItem id = {hike.id} title = {hike.title} summary = {hike.summary} activityLevel = {hike.activityLevel} img = {hike.imgURL} distance = {hike.distance} temp = {hike.temp} clickFunction = {hikeClick}/>
+              </LinkButton>
+            )
+          )}
+          </div>
+        );
+      }
+    } else {
+      toRender = (<div className="loading-text">Loading</div>);
+    }
+    return toRender;
   }
-  
 }
+
+export default TestList;
