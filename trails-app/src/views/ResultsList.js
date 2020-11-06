f03d76bbc4515a1909c721cff9d01b33818d4c76
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-06 10:32:01 -0800

    change to searchObj for rendering

--- trails-app/src/views/ResultsList.js
+++ trails-app/src/views/ResultsList.js
@@ -26,7 +26,7 @@ export default function ResultsList(props) {
   //for (var i = 1; i <= 10; i++){
   //  props.searchObj.results.push(new Hike(i, "Title "+ i, "This is the summary for Hike " + i, i, "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1078&q=80", i, i));
   //}
-  if (results.length == 0){
+  if (props.searchObj.results.length == 0){
     return (
       <div className = 'results-list'>
         <p>No Results Found</p>
@@ -39,7 +39,7 @@ export default function ResultsList(props) {
       // Build results list    
       // for each hike in currentSearch.results render ListItem
       <div className = 'results-list'>
-      {results.map(
+      {props.searchObj.results.map(
         hike => (
           <LinkButton exact to={"/detail-view/" + hike.id}>
             <ListItem id = {hike.id} title = {hike.title} summary = {hike.summary} activityLevel = {hike.activityLevel} img = {hike.imgURL} distance = {hike.distance} temp = {hike.temp} clickFunction = {hikeClick}/>
