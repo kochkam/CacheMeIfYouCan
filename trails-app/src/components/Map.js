ef04fefcd9f7d8ba29728623012afdcd363a4c45
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-07 09:02:22 -0800

    update google map url

--- trails-app/src/components/Map.js
+++ trails-app/src/components/Map.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default function Map(props) {
     var lat = props.searchObj.lat
     var long = props.searchObj.long
-    var mapURL = "https://google.com/maps/@"
-    mapURL += lat + "'" + long +"z"
+    var mapURL = "https://maps.google.com/?q="
+    mapURL += lat + "'" + long
     return(
         <div className="detail-column-container">
             <img src="https://www.xda-developers.com/files/2019/06/google-maps-india.jpg" width="200" height="200" />
