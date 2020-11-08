bf09acc3347d09f68c4c7d0236727c260f3891f1
Peter Strawn <strawnpe@oregonstate.edu>
2020-11-08 15:02:00 -0800

    Update maps link to provide directions

--- trails-app/src/components/Map.js
+++ trails-app/src/components/Map.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default function Map(props) {
     var lat = props.searchObj.results[props.hikeIndex].lat
     var long = props.searchObj.results[props.hikeIndex].long
-    var mapURL = "https://maps.google.com/?q="
-    mapURL += lat + "," + long
+    var mapURL = "https://www.google.com/maps/dir/?api=1&origin="
+    mapURL += props.searchObj.zip + "&destination=" + lat + "," + long
     return(
         <div className="detail-column-container">
             <img src="https://www.xda-developers.com/files/2019/06/google-maps-india.jpg" width="200" height="200" />
