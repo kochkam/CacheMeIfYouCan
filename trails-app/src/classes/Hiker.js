87e3c0437084b825a2f578f3968cd4d14cb937cc
drizzit <mjkochka@gmail.com>
2020-11-11 10:14:30 -0800

    added Hiker class

---
+++ trails-app/src/classes/Hiker.js
@@ -0,0 +1,13 @@
+class Hiker{
+    constructor(firstName,lastName,age,estimatedActivity){
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.estimatedActivity = estimatedActivity
+
+
+
+    }
+
+
+}
