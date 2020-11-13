edcfc64ddfb1230d44c4eb96800c421127d71be1
pittenja <55713063+pittenja@users.noreply.github.com>
2020-11-13 09:56:00 -0800

    jsx formatting changes

--- trails-app/src/views/DetailView.js
+++ trails-app/src/views/DetailView.js
@@ -14,34 +14,36 @@ export default function DetailView(props) {
   console.log(gear.rain);
   return (
     <div>
-    <div className ="detail-row-container" id= {hike.id}>
-        <img className="detail-img" src = {hike.largeimgURL}/>
-        <div className="detail-column-container">
-          <h1 className="detail-text">{hike.title}</h1>
-          <h2 className="detail-text">{hike.summary}</h2>
-          <div className="detail-row-container">
-            <h3 className="detail-text">Difficulty: {hike.activityLevel}</h3>
-            <h3 className="detail-text">Distance: {hike.distance} miles</h3>
+      <div className ="detail-row-container" id= {hike.id}>
+          <img className="detail-img" src = {hike.largeimgURL}/>
+          <div className="detail-column-container">
+            <h1 className="detail-text">{hike.title}</h1>
+            <h2 className="detail-text">{hike.summary}</h2>
+            <div className="detail-row-container">
+              <h3 className="detail-text">Difficulty: {hike.activityLevel}</h3>
+              <h3 className="detail-text">Distance: {hike.distance} miles</h3>
+            </div>
+            <div className="detail-row-container">
+              <h3 className="detail-text">Current Temp: {hike.temp} F</h3>
+              <h3 className="detail-text">Feels Like: {hike.tempFeelsLike} F</h3>
+            </div>
+            <div className="detail-row-container">
+              <h3 className="detail-text">Weather Description: {hike.weather[0].description}</h3>
+            </div>
+            <Map searchObj = {props.searchObj} hikeIndex = {hike.index}/>
           </div>
-          <div className="detail-row-container">
-            <h3 className="detail-text">Current Temp: {hike.temp} F</h3>
-            <h3 className="detail-text">Feels Like: {hike.tempFeelsLike} F</h3>
-          </div>
-          <div className="detail-row-container">
-            <h3 className="detail-text">Weather Description: {hike.weather[0].description}</h3>
-          </div>
-          <Map searchObj = {props.searchObj} hikeIndex = {hike.index}/>
+      </div>
+      <div className = "detail-row-container">
+        <h2>Clothing and Gear Recommendations</h2>
+      </div>
+      <div className = "detail-row-container">
+        <div className = "detail-column-container">
+          <h3>Clothing</h3>
         </div>
-    </div>
-    <div className = "detail-row-container">
-      <h2>Clothing and Gear Recommendations</h2>
-      <div className = "detail-column-container">
-        <h3>Clothing</h3>
+        <div className = "detail-column-container">
+          <h3>Gear</h3>
+        </div>
       </div>
-      <div className = "detail-column-container">
-        <h3>Gear</h3>
-      </div>
-    </div>
     </div>
   );
 }
