9927451c3d88f9b1280b7ce622af197fc968fed9
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-17 14:17:30 -0800

    adjusted css

--- trails-app/src/views/DetailView.js
+++ trails-app/src/views/DetailView.js
@@ -11,77 +11,87 @@ export default function DetailView(props) {
   var gear = new Gear(hike);
 
   return (
-      <div className ="detail-row-container" id= {hike.id}>
-          <img className="detail-img" src = {hike.largeimgURL}/>
-          <div className="detail-column-container">
-            <h1 className="detail-text">{hike.title}</h1>
-            <h2 className="detail-text">{hike.summary}</h2>
-            <div className="detail-row-container">
-              <div className="detail-column-container">
-                <h3>Difficulty: {hike.activityLevel}</h3>
-                <h3>Distance: {hike.distance} miles</h3>
-                <h3>Vertical Ascent: {hike.ascent}</h3>
-              </div>
-              <div className="detail-column-container">
-                <h3>Current Temp: {hike.temp} F</h3>
-                <h3>Feels Like: {hike.tempFeelsLike} F</h3>
-                <h3>Weather Description: {hike.weather[0].description}</h3>
-              </div>
-            </div>
-            <Map searchObj = {props.searchObj} hikeIndex = {hike.index}/>
-            <h2 className="detail-text">Gear Recommendations</h2>
-            {/* Top attribute equation: -5px + (-8px * number of lines after first line) */}
-            <div className = "detail-row-container">
-              <div className = "detail-column-container">
-                <i class="fas fa-hat-cowboy-side tooltip"> Head: {gear.head}
-                  <span class="tooltiptext" style={{top:"-13px"}}>
-                    FILL IN<br/>
-                    TWO LINES
-                  </span>
-                </i>
-                <i class="fas fa-tshirt tooltip"> Top: {gear.top}
-                  <span class="tooltiptext" style={{top:"-13px"}}>
-                    FILL IN<br/>
-                    TWO LINES
-                  </span>
-                </i>
-                <i class="fas fa-socks tooltip"> Bottom: {gear.bottom}
-                  <span class="tooltiptext" style={{top:"-13px"}}>
-                    FILL IN<br/>
-                    TWO LINES
-                  </span>
-                </i>
-                <i class="fas fa-mitten tooltip"> Other: {gear.other}
-                  <span class="tooltiptext" style={{top:"-13px"}}>
-                    FILL IN<br/>
-                    TWO LINES
-                  </span>
-                </i>
-              </div>
-              <div className = "detail-column-container">
-                <i class="fas fa-utensils tooltip"> Food: {gear.calories} calories
-                  <span class="tooltiptext" style={{top:"-61px"}}>
-                    The average number of calories<br/>
-                    you will lose during this hike<br/>
-                    (based on a 160lb person<br/>
-                    walking around 2mph).<br/>
-                    This is equal to about:<br/>
-                    {Math.ceil(gear.calories/105)} banana(s)!<br/>
-                    If you are trying to lose weight,<br/>
-                    you can eat slightly less.
-                  </span>
-                </i>
-                <i class="fas fa-tint tooltip"> Water: {gear.water} oz
-                  <span class="tooltiptext" style={{top:"-29px"}}>
-                    The average amount of water you<br/>
-                    should drink during this hike.<br/>
-                    (based on 6-12oz needed per<br/>
-                    15 minutes of hiking)
-                  </span>
-                </i>
-              </div>
-            </div>
+    <div className ="detail-row-container" id= {hike.id}>
+      <img className="detail-img" src = {hike.largeimgURL}/>
+      <div className="detail-column-container" style={{alignItems:"center"}}>
+        <h1 className="detail-text">{hike.title}</h1>
+        <h2 className="detail-text">{hike.summary}</h2>
+        <div className="detail-row-container">
+          <div className="detail-column-container" style={{textAlign:"right"}}>
+            <h3>Difficulty:</h3>
+            <h3>Distance:</h3>
+            <h3>Vertical Ascent:</h3>
           </div>
+          <div className="detail-column-container" style={{textAlign:"left"}}>
+            <p>{hike.activityLevel}</p>
+            <p>{hike.distance} miles</p>
+            <p>{hike.ascent}</p>
+          </div>
+          <div className="detail-column-container" style={{textAlign:"right"}}>
+            <h3>Current Temp:</h3>
+            <h3>Feels Like:</h3>
+            <h3>Weather:</h3>
+          </div>
+          <div className="detail-column-container" style={{textAlign:"left"}}>
+            <p>{hike.temp} F</p>
+            <p>{hike.tempFeelsLike} F</p>
+            <p>{hike.weather[0].description}</p>
+          </div>
+        </div>
+        <Map searchObj = {props.searchObj} hikeIndex = {hike.index}/>
+        <h2 className="detail-text">Gear Recommendations</h2>
+        {/* Top attribute equation: -5px + (-8px * number of lines after first line) */}
+        <div className = "detail-row-container">
+          <div className = "detail-column-container" style={{textAlign:"left"}}>
+            <i class="fas fa-hat-cowboy-side tooltip"> Head: {gear.head}
+              <span class="tooltiptext" style={{top:"-13px"}}>
+                FILL IN<br/>
+                TWO LINES
+              </span>
+            </i>
+            <i class="fas fa-tshirt tooltip"> Top: {gear.top}
+              <span class="tooltiptext" style={{top:"-13px"}}>
+                FILL IN<br/>
+                TWO LINES
+              </span>
+            </i>
+            <i class="fas fa-socks tooltip"> Bottom: {gear.bottom}
+              <span class="tooltiptext" style={{top:"-13px"}}>
+                FILL IN<br/>
+                TWO LINES
+              </span>
+            </i>
+            <i class="fas fa-mitten tooltip"> Other: {gear.other}
+              <span class="tooltiptext" style={{top:"-13px"}}>
+                FILL IN<br/>
+                TWO LINES
+              </span>
+            </i>
+          </div>
+          <div className = "detail-column-container" style={{textAlign:"left"}}>
+            <i class="fas fa-utensils tooltip"> Food: {gear.calories} calories
+              <span class="tooltiptext" style={{top:"-61px"}}>
+                The average number of calories<br/>
+                you will lose during this hike<br/>
+                (based on a 160lb person<br/>
+                walking around 2mph).<br/>
+                This is equal to about:<br/>
+                {Math.ceil(gear.calories/105)} banana(s)!<br/>
+                If you are trying to lose weight,<br/>
+                you can eat slightly less.
+              </span>
+            </i>
+            <i class="fas fa-tint tooltip"> Water: {gear.water} oz
+              <span class="tooltiptext" style={{top:"-29px"}}>
+                The average amount of water you<br/>
+                should drink during this hike.<br/>
+                (based on 6-12oz needed per<br/>
+                15 minutes of hiking)
+              </span>
+            </i>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
