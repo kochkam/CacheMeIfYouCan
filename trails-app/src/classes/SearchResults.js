526e4f7ab11917cfef614f542aa7480932c2b504
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-17 08:09:33 -0800

    adjusted ascent to new master

--- trails-app/src/classes/SearchResults.js
+++ trails-app/src/classes/SearchResults.js
@@ -44,6 +44,7 @@ class SearchResults{
                 hike.largeimgURL = sorted_results[i].imgMedium;
                 // attribute is titled "length" from api for hike distance. javascript doesnt like this
                 hike.distance = sorted_results[i].length;
+                hike.ascent = response.trails[i].ascent;
                 hike.long = sorted_results[i].longitude;
                 hike.lat = sorted_results[i].latitude;
                 // get temp using weather api
@@ -71,7 +72,6 @@ class SearchResults{
                 hike.largeimgURL = response.trails[i].imgMedium;
                 // attribute is titled "length" from api for hike distance. javascript doesnt like this
                 hike.distance = response.trails[i].length;
-                hike.ascent = response.trails[i].ascent;
                 hike.long = response.trails[i].longitude;
                 hike.lat = response.trails[i].latitude;
                 // get temp using weather api
