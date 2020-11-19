a5f9b431777ea92169023b43b8dba383eafe5340
Peyton Fitzgerald <pdf272@gmail.com>
2020-11-19 18:55:21 -0500

    fixing user profile

--- trails-app/src/views/UserProfile.js
+++ trails-app/src/views/UserProfile.js
@@ -9,7 +9,7 @@ class UserProfile extends Component {
         <p>Use the form below to provide information that will help us in calculating your fitness level.</p>
         <p>Please provide your first name, age, hiking experience, and current exercise frequency.</p>
         <br></br>
-        <FitnessForm fitnessObj = {this.props.fitnessObj}/>
+        <FitnessForm fitnessObj = {this.props.fitnessObj} searchObj = {this.props.searchObj}/>
       </div>
     );
   }
