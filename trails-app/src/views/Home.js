994133d27737526b9b8a48bb60ca262fdece6be7
Ryan Adams <adamsrya@oregonstate.edu>
2020-11-22 13:17:15 -0800

    initial home state setup

--- trails-app/src/views/Home.js
+++ trails-app/src/views/Home.js
@@ -3,14 +3,36 @@ import SearchForm from '../components/SearchForm';
 import AdvSearch from '../components/AdvSearch/AdvSearch';
  
 class Home extends Component {
+
+  constructor(){
+    super();
+    this.state = { displayAdvSearch: false };
+    this.ToggleDisplay = this.ToggleDisplay.bind(this);
+  }
+
+  ToggleDisplay(e) {
+    this.setState({ displayAdvSearch: !this.state.displayAdvSearch });
+
+  }
+
   render() {
-    return (
-      <div className="search-container">
-        <h1>Find your next trail....</h1>
-        <AdvSearch searchObj = {this.props.searchObj}/>
-        <SearchForm searchObj = {this.props.searchObj}/>
-      </div>
-    );
+    if(this.state.displayAdvSearch) {
+      return (
+        <div className="search-container">
+          <h1>Find your next trail....</h1>
+          <button onClick={this.ToggleDisplay} className="FilterToggle">Advanced Search</button>
+          <AdvSearch searchObj = {this.props.searchObj}/>
+        </div>
+      );
+    } else {
+      return (
+        <div className="search-container">
+          <h1>Find your next trail....</h1>
+          <button onClick={this.ToggleDisplay} className="FilterToggle">Advanced Search</button>
+          <SearchForm searchObj = {this.props.searchObj}/>
+        </div>
+      );
+    }
   }
 }
  
