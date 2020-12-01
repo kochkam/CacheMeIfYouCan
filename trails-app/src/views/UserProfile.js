0ce287eefcb0fd8dca1b9c4b99fdb884e5f30f9d
Michael Kochka <michaelkochka@Rachels-MacBook-Pro.local>
2020-11-30 16:37:28 -0800

    Added updates to userprofile.js

--- trails-app/src/views/UserProfile.js
+++ trails-app/src/views/UserProfile.js
@@ -7,6 +7,7 @@ class UserProfile extends Component {
     return (
       <div className="fitness-level-container">
         <h2>Fitness Level Calculator</h2>
+        <br></br>
         <p>Use the form below to provide information that will help us in calculating your fitness level.</p>
         <p>Please provide your first name, age, hiking experience, and current exercise frequency.</p>
         <br></br>
