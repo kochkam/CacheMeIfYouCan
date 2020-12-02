2fbdb372c1c20d04583ab26d3d8a4fbae88b1892
Peter Strawn <strawnpe@oregonstate.edu>
2020-12-02 14:59:58 -0800

    Improve fitness results layout

--- trails-app/src/views/FitnessResultsPage.js
+++ trails-app/src/views/FitnessResultsPage.js
@@ -8,14 +8,24 @@ class FitnessResultsPage extends Component {
     return (
       <div className="fitness-level-container">
         <h2>Fitness Results</h2>
-        <p>This is where the user will see there results.</p>
-        <p>Your name is {this.props.fitnessObj.name}.</p>
-        <p>Your age is {this.props.fitnessObj.age}</p>
-        <p>Your experience is is {this.props.fitnessObj.hikingXP}</p>
-        <p>Your exercise frequency is {this.props.fitnessObj.exerciseFrequency}</p>
-        <p>{this.props.fitnessObj.description}</p>
+        <h4>Thank you, {this.props.fitnessObj.name}, for your submission!</h4>
+        <p>You submitted the following pieces of information:</p>
+        <p>
+        <i>Age</i>: {this.props.fitnessObj.age}
+        </p>
+        <p>
+        <i>Hiking Experience</i>: {this.props.fitnessObj.hikingXP}
+        </p>
+        <p>
+        <i>Exercise Frequency</i>: {this.props.fitnessObj.exerciseFrequency}
+        </p>
         <br></br>
-        <p>Feel free to change your results below</p>
+        <p>Based on that data, here is our recommendation for your hike search:</p>
+        <br></br>
+        <p><b>{this.props.fitnessObj.description}</b></p>
+        <br></br>
+        <p>Feel free to change your results by resubmitting the form. Click the button below to do so.</p>
+        <br></br>
         <button>
         <LinkButton exact to={"/user-profile"}><p>Modify Results</p>
           </LinkButton>
