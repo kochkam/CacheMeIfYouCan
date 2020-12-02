a4b9abcae7bf1602b96b5b8c2c2ec17549de542f
Ryan Adams <adamsrya@oregonstate.edu>
2020-12-02 15:56:01 -0800

    adjusted bold text on advanced search

--- trails-app/src/components/FormComponents/RadioComponent.js
+++ trails-app/src/components/FormComponents/RadioComponent.js
@@ -19,7 +19,7 @@ class RadioComponent extends React.Component {
       render() {
         return (
             <div className="DifficultyFilters">
-                <p>I'm looking for hikes that are... </p>
+                <h3>I'm looking for hikes that are... </h3>
                 <input onChange={this.changeValue} checked={this.props.searchObj.filter.difficultyFilter==="1" || this.state.difficulty === "1"} type="radio" id="easy" name="Difficulty" value="1"/>
                 <label for="easy">Easy</label>
                 <input onChange={this.changeValue} checked={this.props.searchObj.filter.difficultyFilter==="2" || this.state.difficulty === "2"} type="radio" id="medium" name="Difficulty" value="2"/>
