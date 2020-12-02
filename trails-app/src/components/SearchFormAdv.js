1bbe502a05588ccd2f9bd068f7e1a959b87ad025
Michael Kochka <michaelkochka@Rachels-MacBook-Pro.local>
2020-12-02 15:28:38 -0800

    fixed formatting issue

--- trails-app/src/components/SearchFormAdv.js
+++ trails-app/src/components/SearchFormAdv.js
@@ -100,6 +100,7 @@ class SearchFormAdv extends React.Component{
     render() {
         return (
             <form className="searchField" onSubmit={this.onFormSubmit}>
+                <br></br>
                 <div className="DistanceFilters">
                     <h3>I want hikes within the following total length: </h3>
                     <Slider name="distanceChosen" 
@@ -111,9 +112,7 @@ class SearchFormAdv extends React.Component{
                     />
                 </div>
                 <ResultsDropDown searchObj ={this.props.searchObj} resultsChange={this.resultsChange}/>
-                <br></br>
                 <RadioComponent searchObj ={this.props.searchObj} difficultyChange={this.difficultyChange}/>
-                <br></br>
                 <p>Do you also want to see hikes that are easier/harder than your choice above?</p>
                 <p>Leave these boxes unchecked if you only want to see the Hard/Medium/Easy option you selected above.</p>
                 <div className="MinMaxDifficulty">
